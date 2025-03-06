2d3214a8dfdf2cd8aeddabf0cd2cccd8fb0f2a0d
James Duncan <jdun@google.com>
2025-03-06 18:48:24 +0000

    Updating TTL to never expire for pubsub subscriptions
    Also:
    - fix go dependencies
    - update google and google-beta terrform versions to 6.17.0

--- examples/risk/agent/modules/run/jobs.tf
+++ examples/risk/agent/modules/run/jobs.tf
@@ -58,6 +58,9 @@ resource "google_pubsub_subscription" "sub_resp" {
   name                         = "${var.run_job_response}_sub"
   enable_exactly_once_delivery = true
   ack_deadline_seconds         = 60
+  expiration_policy {
+    ttl = ""
+  }
   retry_policy {
     minimum_backoff = "30s"
     maximum_backoff = "600s"
